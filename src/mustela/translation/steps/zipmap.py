9190bf898737dc2130b3e2010bc8072099ebf997
Alessandro Molina <alessandro@molina.fyi>
2025-03-19 09:41:07 +0100

    Reindent zipmap

--- src/mustela/translation/steps/zipmap.py
+++ src/mustela/translation/steps/zipmap.py
@@ -5,21 +5,21 @@ class ZipMapTranslator(Translator):
     # https://onnx.ai/onnx/operators/onnx_aionnxml_ZipMap.html
 
     def process(self):
-            expr = self._variables.consume(self.inputs[0])
-            labels = self._attributes.get("classlabels_strings")
-            if labels:
-                if isinstance(expr, dict):
-                    zipped = {}
-                    keys = list(expr.keys())
-                    for i, label in enumerate(labels):
-                        if i < len(keys):
-                            zipped[label] = expr[keys[i]]
-                    self.set_output(zipped)
-                else:
-                    self.set_output({
-                        label: expr for label in labels
-                    })
+        expr = self._variables.consume(self.inputs[0])
+        labels = self._attributes.get("classlabels_strings")
+        if labels:
+            if isinstance(expr, dict):
+                zipped = {}
+                keys = list(expr.keys())
+                for i, label in enumerate(labels):
+                    if i < len(keys):
+                        zipped[label] = expr[keys[i]]
+                self.set_output(zipped)
             else:
-                # int64 class labels,
-                # for the moment assume our data is aldeary numeric
-                self.set_output(expr)
+                self.set_output({
+                    label: expr for label in labels
+                })
+        else:
+            # int64 class labels,
+            # for the moment assume our data is aldeary numeric
+            self.set_output(expr)

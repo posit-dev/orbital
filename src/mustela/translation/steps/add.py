f7f21245375c8e6f920eeb4d6b84a4909f202d74
Alessandro Molina <alessandro@molina.fyi>
2025-03-20 11:29:46 +0100

    cleanup Add refactor

--- src/mustela/translation/steps/add.py
+++ src/mustela/translation/steps/add.py
@@ -28,21 +28,5 @@ class AddTranslator(Translator):
                 "The second input (initializer) must contain exactly 1 value when the first operand is not concatenated."
             )
             self._variables[self._output_name] = self._optimizer.fold_operation(
-                first_operand - add_values[0]
+                first_operand + add_values[0]
             )
-
-    def _process(self):            
-        # TODO: Fix by copying Sub design
-        intercept = self._variables.get_initializer_value(self.inputs[1])[0]
-        first_operand = self._variables.consume(self.inputs[0])
-
-        if isinstance(first_operand, dict):
-            struct_fields = list(first_operand.keys())
-            self.set_output({
-                field: first_operand[field] + intercept
-                for i, field in enumerate(struct_fields)
-            })
-        else:
-            self.set_output(self._optimizer.fold_operation(
-                first_operand + intercept
-            ))
